def9dc53258f6bb6285fe3bc986c7a590afd4075
zulo <zulo191@gmail.com>
2025-08-16 15:17:09 -0300

    adicionei um espaço

--- script.py
+++ script.py
@@ -5,4 +5,4 @@ media = (nota1+nota2)/2
 if media > 60:
     print(f'sua nota é {media} você foi aprovado')
 else:
-    print(f'sua nota é{media} e você foi repeovado')
+    print(f'sua nota é {media} e você foi repeovado')
