306b80e4ed29442908e69eb9b90f2ca7c391402b
zulo <zulo191@gmail.com>
2025-08-16 16:19:32 -0300

    adicionei um espaço

--- estudo de casa.py
+++ estudo de casa.py
@@ -2,14 +2,13 @@ nota1 = int(input('qual a sua nota do 1° semestre \n'))
 nota2 = int(input('qual a sua nota do 2° semestre \n'))
 media = (nota1+nota2)/2
 
-calcular_media = True
+# if: se for tal coisa
+# else: se não for tal coisa
 
-if nota1>100 or nota2>100:
-    calcular_media=False
-if calcular_media:
-    if media > 60:
+if nota1<=100 and nota2<=100:
+    if media >= 60:
         print(f'sua nota é {media} você foi aprovado')
     else:
         print(f'sua nota é {media} e você foi repeovado')
 else:
-    print('\n as notas não podem ser maiores que 100 \n' )
+    print('\n as notas não podem ser maiores que 100 \n')
