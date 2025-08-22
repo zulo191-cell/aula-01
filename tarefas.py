59298f0336ee4b038077c3ebe49d8b97f41c5429
zulo <zulo191@gmail.com>
2025-08-22 19:47:38 -0300

    tarefa

---
+++ tarefas.py
@@ -0,0 +1,7 @@
+#nome = str (input(f"Qual o seu nome: \n"))
+#sobrenome = str (input(f'Qual so seu sobrenome: \n'))
+#telefone = int (input(f'Qual o seu numero:'))
+
+#print('Ola', nome, sobrenome)
+#print('Seu numero é:', telefone)
+
