12b1390e7a7b09361e88e5d021cb040afdf3fd6a
zulo <zulo191@gmail.com>
2025-08-24 18:32:20 -0300

    tarefa

--- tarefas.py
+++ tarefas.py
@@ -5,3 +5,20 @@
 #print('Ola', nome, sobrenome)
 #print('Seu numero é:', telefone)
 
+#var_str = '100'
+
+#var_str = int(var_str)
+#var_str = float(var_str)
+#print(type(var_str))
+
+
+#num = 35
+#num = float(num)
+#print(type(num))
+
+#num1 =int(input('primeiro numero:\n'))
+#num2 =int(input('segundo numero:\n'))
+#print('resultado da soma:', num1 + num2 )
+#print('resultado da divisão:', num1 / num2 )
+#print('resultado da mutiplicação:', num1 * num2 )
+#print('resultado da subitração:', num1 - num2 )
