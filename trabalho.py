cf1738cdc9904b98dedfacca603f43156987916e
zulo <zulo191@hmail.com>
2025-09-19 23:44:04 -0300

    trabalho dia 19/09/2025

---
+++ trabalho.py
@@ -0,0 +1,109 @@
+#exercicio 1
+# numero1= int and float(input("Digite o primeiro numero:\n"))
+# numero2= int and float(input("Digite o segundo numero:\n"))
+#
+# if numero1 == numero2:
+#     print("São numeros iguais")
+# elif numero1 < numero2:
+#     print("O segundo numero é maior")
+# else:
+#     print("O primeiro numero é maior")
+
+#exercicio 2
+
+# idade=int(input("Qual a sua idade:\n"))
+#
+# if idade >= 18 :
+#     print("Você ja pode votar e dirigir")
+# elif idade >= 16:
+#     print("Você ja pode votar mais não dirigir ")
+# else:
+#     print("Você é de menor e não pode nem dirigir ou votar")
+
+
+#exercicio 3
+
+# numero = float(input("Digite um número: "))
+
+# if numero > 0:
+#     print("O número é positivo.")
+# elif numero < 0:
+#     print("O número é negativo.")
+# else:
+#     print("O número é zero.")
+
+
+#exercicio 4
+
+# numero1= int(input("Digite o numero: "))
+# numero2= int(input("Digite o numero: "))
+
+# if (numero1%2== 0) and (numero2%2== 0) :
+#     print("Ambos os números são pares.")
+# else:
+#     print("Pelo menos um ou os dois números não são pares.")
+
+
+#exercicio 5
+
+# numero = int(input("Digite um número: "))
+
+# if 10 <= numero <= 50:
+#     print(f"O número {numero} está entre 10 e 50.")
+# else:
+#     print(f"O número {numero} não está entre 10 e 50.")
+
+#exercicio 6
+
+
+# numero1 = int(input("Por favor, insira o primeiro número: "))
+# numero2 = int(input("Por favor, insira o segundo número: "))
+
+# if numero1 > 100 or numero2 > 100:
+#     print("Pelo menos um dos números é maior que 100.")
+# else:
+#     print("Nenhum dos números é maior que 100.")
+
+
+#exercicio 7
+
+# nota = int and float(input("Qual a sua nota:\n"))
+# if nota >=60 :
+#     print("Aprovado")
+# elif nota >= 40 and nota <= 59.9:
+#     print("Recuperação")
+# else :
+#     print("Reprovado")
+
+#exercicio 8
+
+# idade = int(input("Qual a sua idade:\n"))
+# if idade < 12 :
+#     print("criança")
+# elif idade >= 12 and idade < 17:
+#     print("Adolescente")
+# elif idade >= 18 and idade < 59:
+#     print("Adulto")
+# else :
+#     print("idoso")
+
+
+#exercicio 9
+
+# login=str(input(f"qual o seu login:\n"))
+# senha=int(input(f"digite a senha:\n"))
+
+# if senha == 1234 and login == "adm":
+#     print("acesso permitido")
+# else:
+#     print("acesso negado")
+
+#exercicio 10
+
+# temperatura = int(input("Qual a temperatura:\n"))
+# if temperatura < 15:
+#     print("Está Frio")
+# elif temperatura >= 15 and temperatura < 30:
+#     print("Está agradável")
+# else:
+#     print("Está quente")
